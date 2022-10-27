867afab60568408d39ea3983bdd541a603ac6a88
apran42 <flyingdisc1@naver.com>
2022-10-28 03:38:54 +0900

    Create 10170_NFC West vs North.c

---
+++ Bronze/C/10170_NFC West vs North.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("NFC West       W   L  T");
+    puts("-----------------------");
+    puts("Seattle        13  3  0");
+    puts("San Francisco  12  4  0");
+    puts("Arizona        10  6  0");
+    puts("St. Louis      7   9  0");
+    puts("");
+    puts("NFC North      W   L  T");
+    puts("-----------------------");
+    puts("Green Bay      8   7  1");
+    puts("Chicago        8   8  0");
+    puts("Detroit        7   9  0");
+    puts("Minnesota      5  10  1");
+    return 0;
+}
