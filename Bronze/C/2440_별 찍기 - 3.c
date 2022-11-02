c10ca6ddff1ee013eb1baf2763cc2e2300c3afd4
apran42 <flyingdisc1@naver.com>
2022-11-02 11:55:57 +0900

    22.11.02

---
+++ Bronze/C/2440_별 찍기 - 3.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        for(int j=n; j>i; j--)
+            printf("*");
+        puts("");
+    }
+    return 0;
+}
