be8709d06761fb201679893c71281e1b3eb90de1
apran42 <flyingdisc1@naver.com>
2023-05-16 17:07:39 +0900

    Update 24313_알고리즘 수업 - 점근적 표기 1.c
    문제 해결

--- Silver/C/24313_알고리즘 수업 - 점근적 표기 1.c
+++ Silver/C/24313_알고리즘 수업 - 점근적 표기 1.c
@@ -1,1 +1,14 @@
-// 아직 해결 중
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a1, a0, c, n0;
+	scanf("%d %d", &a1, &a0);
+	scanf("%d", &c);
+	scanf("%d", &n0);
+
+	if ((double)(a1 * n0 + a0) / (double)(c * n0) <= 1 && (c - a1 >= 0))
+		puts("1");
+	else
+		puts("0");
+}
