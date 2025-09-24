6f7b00c9d02796a970bf2426f8ca0bafc89cf839
apran42 <flyingdisc1@naver.com>
2025-09-24 12:00:57 +0900

    BOJ-EX: 2025. 9. 24. 오후 12:00:55

--- 1735번 - 분수 합/분수 합.py
+++ 1735번 - 분수 합/분수 합.py
@@ -1,20 +1,31 @@
 #  **************************************************************************  #
 #                                                                              #
 #                                                       :::    :::    :::      #
-#    Problem Number: 11478                             :+:    :+:      :+:     #
+#    Problem Number: 1735                              :+:    :+:      :+:     #
 #                                                     +:+    +:+        +:+    #
 #    By: flyingdisc1 <boj.kr/u/flyingdisc1>          +#+    +#+          +#+   #
 #                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11478                          #+#        #+#      #+#     #
-#    Solved: 2025/09/24 11:00:13 by flyingdisc1   ###          ###   ##.kr     #
+#    https://boj.kr/1735                           #+#        #+#      #+#     #
+#    Solved: 2025/09/24 11:30:18 by flyingdisc1   ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
 import sys
 
-string = list(sys.stdin.readline().rstrip())
+def gcd(a,b):
+	a, b = max(a,b), min(a,b)
+	while a!=0 and b!=0:
+		a, b = b, a%b
+	return a
 
-string_set = set()
-for i in range(len(string)):
-    for j in range(len(string)-i):
-        string_set.add(''.join(string[j:j+i+1]))
-sys.stdout.write(str(len(string_set)))
+bunsu1 = [0, 0]
+bunsu2 = [0, 0]
+bunsu1[0], bunsu1[1] = map(int, sys.stdin.readline().split())
+bunsu2[0], bunsu2[1] = map(int, sys.stdin.readline().split())
+bunmo = bunsu1[1]*bunsu2[1]
+bunja = bunsu1[0]*bunsu2[1] + bunsu1[1]*bunsu2[0]
+
+gongyaksu = gcd(bunmo, bunja)
+bunmo //= gongyaksu
+bunja //= gongyaksu
+
+print(bunja, bunmo)
