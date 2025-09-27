ce0a316bed12225c5a5d39011984b16ddb7ad161
apran42 <flyingdisc1@naver.com>
2025-09-28 06:24:40 +0900

    BOJ-EX: 2025. 9. 28. 오전 6:24:39

--- 4949번 - 균형잡힌 세상/균형잡힌 세상.py
+++ 4949번 - 균형잡힌 세상/균형잡힌 세상.py
@@ -11,3 +11,48 @@
 #  **************************************************************************  #
 import sys
 
+class Stack:
+    def __init__(self):
+        self.bracket = []
+    # 괄호 추가
+    def add(self, b):
+        self.bracket.append(b)
+    # 괄호 삭제
+    def erase(self):
+        if len(self.bracket) != 0:
+            return self.bracket.pop()
+        else:
+            return None
+    # 길이를 반환, 비었으면 True, 있으면 False    
+    def is_empty(self):
+        return len(self.bracket) == 0
+    
+while True:
+    string = sys.stdin.readline().rstrip()
+    if string == '.':
+        break
+    bracket = Stack()
+    
+    cond = True
+    for char in string:
+        # 여는 괄호 추가
+        if char == '(' or char == '[':
+            bracket.add(char)
+        # 닫는 괄호일 경우에
+        if char == ')':
+            if bracket.erase() == '(':
+                continue
+            else:
+                cond = False
+                break
+        if char == ']':
+            if bracket.erase() == '[':
+                continue
+            else:
+                cond = False
+                break
+    # 조건에 만족
+    if bracket.is_empty() and cond == True:
+        sys.stdout.write('yes\n')
+    else:
+        sys.stdout.write('no\n')
