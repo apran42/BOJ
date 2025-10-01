724e20c8f4924cddfa597841329a31e21c060758
apran42 <flyingdisc1@naver.com>
2025-10-02 00:40:05 +0900

    BOJ-EX: 2025. 10. 2. 오전 12:40:04

--- 32172번 - 현권이와 신기한 수열/현권이와 신기한 수열.py
+++ 32172번 - 현권이와 신기한 수열/현권이와 신기한 수열.py
@@ -1,24 +1,27 @@
 #  **************************************************************************  #
 #                                                                              #
 #                                                       :::    :::    :::      #
-#    Problem Number: 28323                             :+:    :+:      :+:     #
+#    Problem Number: 32172                             :+:    :+:      :+:     #
 #                                                     +:+    +:+        +:+    #
 #    By: flyingdisc1 <boj.kr/u/flyingdisc1>          +#+    +#+          +#+   #
 #                                                   +#+      +#+        +#+    #
-#    https://boj.kr/28323                          #+#        #+#      #+#     #
-#    Solved: 2025/10/01 15:27:01 by flyingdisc1   ###          ###   ##.kr     #
+#    https://boj.kr/32172                          #+#        #+#      #+#     #
+#    Solved: 2025/10/01 23:59:36 by flyingdisc1   ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
 import sys
 
 n = int(sys.stdin.readline())
-numbers = list(map(int, sys.stdin.readline().split()))
-even, odd = 0, 0
 
-for num in numbers:
-    if num%2 == 0: # 짝수면
-        even = odd + 1
-    else: # 홀수면
-        odd = even + 1
-answer = max(odd, even)
-sys.stdout.write(str(answer))    
+nums = set()
+# 초기값
+nums.add(0)
+num = 0
+for i in range(1, n+1):
+    # num 계산
+    num = num - i
+    if num < 0 or num in nums:
+        num += i*2
+    # nums에 num추가
+    nums.add(num)
+sys.stdout.write(str(num))
